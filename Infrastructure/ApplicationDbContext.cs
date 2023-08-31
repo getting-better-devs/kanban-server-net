88cb14e26c17934a71446109ddbb81aea53bc8fa
Carlos Martins <lemos_oliveiraa@hotmail.com>
2023-08-31 19:04:50 -0300

    [chore] Layer configuration and start of repository layer configuration

---
+++ Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions options) 
+        : base(options)
+    { }
+    
+    public required DbSet<Board> Boards { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+    }
+}
---
+++ Infrastructure/Bootstrapper.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure;
+
+public static class Bootstrapper
+{
+    public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        AddDbContext(services);
+    }
+    
+    private static void AddDbContext(IServiceCollection services) 
+    {
+        services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            options.UseNpgsql("Host=localhost;Database=postgres;Username=postgres;Password=postgres");
+        });
+    }
+}
