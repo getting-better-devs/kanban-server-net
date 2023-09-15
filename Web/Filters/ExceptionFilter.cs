c4e2569b667cbd3d73cdb235df58c865361e1b71
Carlos Martins <lemos_oliveiraa@hotmail.com>
2023-09-15 17:20:02 -0300

    [chore] Created exception filter

---
+++ Web/Filters/ExceptionFilter.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+using Domain.Primitives.Result.ErrorResult;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Web.Filters;
+
+public class ExceptionFilter : IExceptionFilter
+{
+    private readonly IHostEnvironment _env;
+
+    public ExceptionFilter(IHostEnvironment env)
+    {
+        _env = env;
+    }
+    
+    public void OnException(ExceptionContext context)
+    {
+        var message = _env.IsDevelopment()
+            ? context.Exception.StackTrace ?? string.Empty
+            : "An unknown error occurred.";
+        
+        context.HttpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+        context.Result = new ObjectResult(new ErrorResult(message));
+    }
+}
